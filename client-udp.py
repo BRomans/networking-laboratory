2b5143e015cf2c806ba1d8b1709a2aeb2ce4afcb
Bromans <michele.romani.gzl0@me.com>
2018-04-06 18:35:13 +0200

    client and server using udp protocol

---
+++ client-udp.py
@@ -0,0 +1,50 @@
+
+import socket   # for sockets
+import sys  # for exit
+
+
+
+
+
+try:
+    # specifica che vogliamo creare un socket udp
+    s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+except socket.error, msg:
+    print 'Failed to create socket. Error code: ' + str(msg[0]) + ' , Error message : ' + msg[1]
+    sys.exit()
+
+print 'Socket Created'
+
+# Resolve hostname
+host = '127.0.0.1'
+
+try:
+    UDP_IP = socket.gethostbyname( host )
+
+except socket.gaierror:
+    # could not resolve
+    print 'Hostname could not be resolved. Exiting'
+    sys.exit()
+
+
+
+UDP_PORT = 8888
+MESSAGE = "My udp message"
+try:
+    # con udp non c'e' bisogno di connettersi al server, non esiste handshake
+
+    s.sendto(MESSAGE, (UDP_IP, UDP_PORT))
+except socket.error:
+    #Send failed
+    print 'Send failed'
+    sys.exit()
+
+print 'Message send successfully'
+
+
+# Now receive data
+reply = s.recv(4096)
+
+print reply
+
+s.close()
---
+++ server-udp.py
@@ -0,0 +1,32 @@
+import socket
+import sys
+
+UDP_IP = '127.0.0.1'   # Symbolic name meaning all available interfaces
+UDP_PORT = 8888 # Arbitrary non-privileged port
+
+sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+
+print 'Socket created'
+
+try:
+    #dice al s.o. di aprire la connessione
+    sock.bind((UDP_IP, UDP_PORT))
+except socket.error, msg:
+    print 'Bind failed. Error code : ' + str(msg[0]) + ' Message ' + msg[1]
+    sys.exit()
+
+print 'Socket bind complete'
+
+
+print 'Socket now listening'
+
+while True:
+    #con udp non serve accettare richieste di connessione, si ricevono direttamente i dati
+    data, addr = sock.recvfrom(1024)
+
+    print "received message:", data
+    reply = 'OK...' + data
+    if not data or data.rstrip() == "ciao":
+        break
+
+sock.close()
