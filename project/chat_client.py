ed395f4c4c5c04acccb7bc8ec79d12197f1e3c02
BRomans <michele.romani.gzl0@gmail.com>
2018-04-25 14:02:18 +0200

    - Manage connection to server and answers to command if not connected. Add documentation for client script.

--- project/chat_client.py
+++ project/chat_client.py
@@ -17,7 +17,9 @@ import sys
 import thread
 
 
-
+'''
+Switch the value of the global variable inChat that defines if the client user is chatting or not.
+'''
 
 def switchInChat():
     global inChat
@@ -27,19 +29,29 @@ def switchInChat():
     else:
         print('Swap to command mode, pres <enter> to continue...\n')
 
-
+'''
+Return the value of the global variable inChat
+'''
 def getInChat():
     global inChat
     return inChat
 
+'''
+Set the address and the port of the target user for chat as global variables
+'''
 def setTargetAddress(address, port):
     global targetAddress
     global targetPort
     targetAddress = address
     targetPort = port
 
+'''
+Register to the chat server. This function is automatically called the first time the client is started.
+It can be re called with !reconnect command.
+'''
 def registerToServer(command, username, userIpAddress, userPort):
-
+    global connected
+    connected = True
     try:
         #create an AF_INET, STREAM socket (TCP)
         clientSocket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
@@ -66,7 +78,10 @@ def registerToServer(command, username, userIpAddress, userPort):
         #Send failed
         print ('Send failed. Error Code : ' + str(msg[0]) + ' Message ' + msg[1])
 
-
+'''
+Deprecated.
+Retrieve the ip address of the specified user. This function utility has been replaced by retrieveUserInfo.
+'''
 def retrieveUserIp(name):
     try:
         #create an AF_INET, STREAM socket (TCP)
@@ -75,7 +90,6 @@ def retrieveUserIp(name):
         print 'Failed to create socket. Error code: ' + str(msg[0]) + ' , Error message : ' + msg[1]
         sys.exit()
     clientSocket.connect((remote_ip , port))
-    #Send some data to remote server
     connection_message = ('User ' + name + ' is connecting...\r\n\r\n')
     print ('Socket Connected to ' + host + ' on ip ' + remote_ip)
     try :
@@ -92,9 +106,13 @@ def retrieveUserIp(name):
         #Send failed
         print 'Send failed. Error Code : ' + str(msg[0]) + ' Message ' + msg[1]
 
+'''
+Tells the server that the client user is disconnecting.
+'''
 def sendQuitSignal(username):
-    command = 'quit'
+    global connected
     connected = False
+    command = 'quit'
     try:
         #create an AF_INET, STREAM socket (TCP)
         clientSocket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
@@ -114,6 +132,9 @@ def sendQuitSignal(username):
         #Send failed
         print 'Send failed. Error Code : ' + str(msg[0]) + ' Message ' + msg[1]
 
+'''
+Retrieve list of online users.
+'''
 def retrieveUserList():
     command = 'userList'
     try:
@@ -135,6 +156,9 @@ def retrieveUserList():
         #Send failed
         print 'Send failed. Error Code : ' + str(msg[0]) + ' Message ' + msg[1]
 
+'''
+Retrieve all the info of a specified user. These include username, ip address and udp port.
+'''
 def retrieveUserInfo(name):
     command = 'getUser'
     try:
@@ -160,6 +184,9 @@ def retrieveUserInfo(name):
         print 'Could not connect to user. Error Code : ' + str(msg[0]) + ' Message ' + msg[1]
         return ''
 
+'''
+Print an user manual on screen.
+'''
 def printHelpManual():
     print ('\n--------------------------------------------------------------------\n'
            '-- Hello, here you are a quick guide for the chat application:    --\n'
@@ -176,7 +203,10 @@ def printHelpManual():
            '-- !quit : close the client application                           --\n'
            '---------------------------------------------------------------------\n')
 
-
+'''
+Starts the udp server of the client, listening for chat messages from other clients. This function is called in a
+parallel thread.
+'''
 def startUpdServer():
     print ('\nListening for chat requests...\n')
     udp_socket.bind(('', userPort))
@@ -208,7 +238,9 @@ def startUpdServer():
         except socket.error:
             pass
 
-
+'''
+Initialize a new chat and switch the client status to inChat.
+'''
 def initChat(userInfo):
     global targetAddress
     global targetPort
@@ -219,28 +251,35 @@ def initChat(userInfo):
     sendChatMessage('!start', targetAddress, targetPort)
     switchInChat()
 
-
+'''
+Send a single message over udp to the specified address.
+'''
 def sendChatMessage(message, address, port):
-
     try:
         # directly send to udp server
         userMessage = message
         udp_socket.sendto(userMessage, (address, port))
-
     except socket.error:
         #Send failed
         print 'Error, could not deliver the message...\n'
 
 
-
+'''
+Initial setup of the client.
+Username, user address and udp port are taken from command line arguments.
+The host is set to default to 127.0.0.1. At this time there is not yet a dynamic
+configuration of the server address.
+When the client starts, it automatically connects to the server and start the
+udp socket in a parallel thread.
+'''
 userIpdAddress = str(sys.argv[1])
 userPort = int(sys.argv[2])
 username = str(sys.argv[3])
-connected = False
 inChat = False
 userList = []
 global targetAddress
 global targetPort
+global connected
 
 # Resolve hostname
 host = '127.0.0.1'
@@ -268,6 +307,12 @@ udp_socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 udp_socket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
 thread.start_new_thread(startUpdServer, ())
 
+
+'''
+Endless loop that asks the user for commands to perfrom specific actions.
+If the state is 'inChat' then every input of the user becomes a chat message
+until the chat session is not over.
+'''
 while 1:
     if not getInChat():
         print ('\n\nPlease, choose a command from the list:\n'
@@ -288,22 +333,27 @@ while 1:
 
     if user_input[:9] == "!connect ":
         name = user_input[9:].rstrip()
+        if name == username:
+            print ('You cannot chat with yourself SmartBoy ;) ...\n')
+            continue
         try:
             info = retrieveUserInfo(name)
-            if info:
+            if info and info != 'notFound' and connected:
                initChat(info)
             else:
-                print('Error in starting chat...\n')
+                print('Could not start chat...\n')
         except:
             print ('Error in starting chat...\n')
     elif user_input == '!connect':
-            #name = raw_input('Choose an username to start a new chat: ')
-            name = raw_input('Select user to start chat: ')
-            info = retrieveUserInfo(name)
-            if info and info != 'notFound':
-                initChat(info)
-            else:
-                print('Error in starting chat...\n')
+        name = raw_input('Select user to start chat: ')
+        if name == username:
+            print ('You cannot chat with yourself SmartBoy ;) ...\n')
+            continue
+        info = retrieveUserInfo(name)
+        if info and info != 'notFound' and connected:
+            initChat(info)
+        else:
+            print('Error in starting chat...\n')
 
     elif user_input == '!reconnect':
         sendQuitSignal(username)
@@ -311,21 +361,29 @@ while 1:
         registerToServer(command, username, userIpdAddress, userPort)
 
     elif user_input == '!disconnect':
-        sendQuitSignal(username)
-        print ('Client disconnected from server...\n')
-
+        if connected:
+            sendQuitSignal(username)
+            print ('Client disconnected from server...\n')
+        else:
+            print ('Already disconnected...\n')
     elif user_input == '!users':
-        print ('Users retrieved!\n')
-        userList = retrieveUserList()
-
+        if connected:
+            print ('Users retrieved!\n')
+            userList = retrieveUserList()
+        else:
+            print ('You must connect to server first...\n')
     elif user_input[:5] == "!get ":
-        name = user_input[5:].rstrip()
-        retrieveUserInfo(name)
-
+        if connected:
+            name = user_input[5:].rstrip()
+            retrieveUserInfo(name)
+        else:
+            print ('You must connect to server first...\n')
     elif user_input == '!get':
-        name = raw_input('Choose an username: ')
-        retrieveUserInfo(name)
-
+        if connected:
+            name = raw_input('Choose an username: ')
+            retrieveUserInfo(name)
+        else:
+            print ('You must connect to server first...\n')
     elif user_input == '!help':
         printHelpManual()
 
